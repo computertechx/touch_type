bc97bc33ad5f9e2c31d8972ba5456bb7b4bd9c8c
feroz s <computertechx@gmail.com>
2023-05-21 03:00:52 -0400

    added typing.py. just comments baseline so far

---
+++ typing.py
@@ -0,0 +1,13 @@
+'''
+A typiny tutor/traininy
+progran, no install,
+ copy, paste and run(CLI)
+VERSION 1.0 ALPHA
+'''
+# REQUIRED IMPORTS
+
+#DECLARE AND ASSIGN VARIABLE
+
+# CORE FUNCTIONS
+
+# MAIN PROGRAN LOOP
